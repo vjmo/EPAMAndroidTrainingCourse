4c909c3d34df5a37cf5785bb1c098a1b785f613a
vjmo <46070493+vjmo@users.noreply.github.com>
2021-05-15 16:45:11 +0300

    [lection_4] Homework for lesson 4

---
+++ lection_4/kotlin/Function.kt
@@ -0,0 +1,17 @@
+typealias Ui = UserUiModel
+
+fun User.printInfo(numberOfSpaces: Int, printing: (String)->Unit) {
+
+    println("Name: $name Surname: $surname$ Age: $age$ Email: $email$ Phone: $numberPhone\n")
+    printing("Name: $name Surname: $surname$ Age: $age$ Email: $email$ Phone: $numberPhone\n")
+}
+
+fun User.toUserUiModel() = Ui(
+    name = "$name",
+    surname = "$surname"
+)
+
+inline fun User.printInfoInline(numberOfSpaces: Int, printing: (String)->Unit){
+    println("Name: $name Surname: $surname$ Age: $age$ Email: $email$ Phone: $numberPhone\n")
+    printing("Name: $name Surname: $surname$ Age: $age$ Email: $email$ Phone: $numberPhone\n")
+}
---
+++ lection_4/kotlin/User.kt
@@ -0,0 +1,3 @@
+data class User(var id: Int, var name: String,
+                var surname: String, var age: Int,
+                var email: String, var numberPhone: Int)
