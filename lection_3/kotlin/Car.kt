b6e6be551081a569a9f6cbce69de46399478dc44
vjmo <46070493+vjmo@users.noreply.github.com>
2021-05-15 16:21:35 +0300

    [lection_3] Homework for lesson 3.

---
+++ lection_3/kotlin/Car.kt
@@ -0,0 +1,11 @@
+class Car(color: Color, name: String, numberOfWheels: Int, maxPeople: Int, var transmission: Boolean) :
+    Transport(color, name, numberOfWheels, maxPeople) {
+    fun infoTransmission(){
+        println("Mechanical = true; Automatic = false")
+    }
+    override fun printInfoTransport() {
+        println("Color: " + color + " Name transport: " + name + " Number Of Wheels: "
+                + numberOfWheels + " Max people: " + maxPeople + " Transmission: "+ transmission)
+    }
+
+}
